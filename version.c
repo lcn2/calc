fb4a03c1f1694a3d846034077572eb4e5f04967e
Landon Curt Noll <github-mail@asthe.com>
2006-09-18 01:05:19 -0700

    Release calc version 2.12.1.4

--- version.c
+++ version.c
@@ -19,8 +19,8 @@
  * received a copy with calc; if not, write to Free Software Foundation, Inc.
  * 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
  *
- * @(#) $Revision: 29.69 $
- * @(#) $Id: version.c,v 29.69 2006/09/18 06:29:07 chongo Exp $
+ * @(#) $Revision: 29.70 $
+ * @(#) $Id: version.c,v 29.70 2006/09/18 08:01:36 chongo Exp $
  * @(#) $Source: /usr/local/src/cmd/calc/RCS/version.c,v $
  *
  * Under source code control:	1990/05/22 11:00:58
@@ -48,7 +48,7 @@ static char *program;
 #define MAJOR_VER	2	/* major version */
 #define MINOR_VER	12	/* minor version */
 #define MAJOR_PATCH	1	/* patch level or 0 if no patch */
-#define MINOR_PATCH	3	/* test number or 0 if no minor patch */
+#define MINOR_PATCH	4	/* test number or 0 if no minor patch */
 
 
 /*
