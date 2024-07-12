232b3bddef267776613e23e325bdc949d8a90e79
Landon Curt Noll <github-mail@asthe.com>
2024-07-11 22:49:00 -0700

    Release v2.15.1.0
    The following are the changes in this release:
    
        Converted all ASCII tabs to ASCII spaces using a 8 character
        tab stop, for all files, except for all Makefiles (plus rpm.mk).
        The command `git diff -w` reports no changes.  There is no
        functionality change in calc: only ASCII tabs to ASCII spaces.
    
        Fixed trailblank.  It was pruning . in its find search.
        Added check for ASCII tabs is non-Makefiles.
    
        This version will form the basis for the calc v2 to calc v3 fork.

--- version.h
+++ version.h
@@ -63,8 +63,8 @@
  */
 #define MAJOR_VER       2       /* level 1: major library version */
 #define MINOR_VER       15      /* level 2: minor library version */
-#define MAJOR_PATCH     0       /* level 3: major software version level */
-#define MINOR_PATCH     7       /* level 4: minor software version level */
+#define MAJOR_PATCH     1       /* level 3: major software version level */
+#define MINOR_PATCH     0       /* level 4: minor software version level */
 
 
 #endif /* !INCLUDE_VERSION_H*/
